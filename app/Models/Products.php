0cf8b36f6b24fd7be478d187a7c46aa5d1abc1c5
jdu211171 <211171m@jdu.uz>
2025-03-10 18:40:04 +0500

    Products Model

---
+++ app/Models/Products.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductsFactory> */
+    use HasFactory;
+    protected $fillable =
+        [
+            'product_id',
+            'name',
+            'description',
+            'price',
+            'sale_price',
+            'category_id',
+            'product_volume',
+            'stock_quantity',
+            'created_at',
+            'updated_at'
+        ];
+}
