32cb3ee5be5abec0d98bac1367c10657b04d9ffa
YoqubovDev <xiyobidasturchi0011@gmail.com>
2025-03-11 08:11:15 +0500

    Patch: new Model and Resources

--- app/Models/ProductVolume.php
+++ app/Models/ProductVolume.php
@@ -4,12 +4,20 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class ProductVolume extends Model
 {
     protected $fillable = [
         'name',
     ];
+    protected static function boot()
+    {
+        parent::boot();
+        static ::creating(function ($model){
+            $model->slug=Str::slug($model->name);
+        });
+    }
 
     public function products()
     {
