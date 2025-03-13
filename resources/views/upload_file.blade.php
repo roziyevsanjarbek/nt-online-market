f26549e0621499f6710fb04de71c5815cbf4b8a4
YoqubovDev <xiyobidasturchi0011@gmail.com>
2025-03-13 17:55:18 +0500

    Delete Upload_file

--- resources/views/upload_file.blade.php
+++ resources/views/upload_file.blade.php
@@ -1,18 +1,23 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload File</title>
 </head>
 <body>
+<h2>Upload Image or File</h2>
+<form action="{{ route('upload') }}" method="post" enctype="multipart/form-data">
+    @csrf
+    <input type="file" name="file" id="file">
+    <button type="submit">Upload</button>
+</form>
 
-    <form action="/upload" method="post" enctype="multipart/form-data">
-        @csrf
-        <input type="file" name="file">
-        <button type="submit">Upload</button>
-    </form>
+@if (session('success'))
+    <p>{{ session('success') }}</p>
+    @if (session('relativePath'))
+        <img src="{{ asset('storage/' . session('relativePath')) }}" alt="Uploaded Image" style="max-width: 500px;">
+    @endif
+@endif
 </body>
 </html>
