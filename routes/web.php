61e5df32eb7f08994d9b8be0097b48dbb3795287
Fazliddin <salohiddinovfazliddin265@gmail.com>
2025-03-13 11:33:08 +0500

    upload_file

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/upload_file', function () {
+    return view('upload_file');
+});
+Route::post('upload_file', function () {
+    $request = request();
+    $file = $request->file('file');
+    $file_path = $file->store('uploads', 'public');
+    dd($file_path);
+});
