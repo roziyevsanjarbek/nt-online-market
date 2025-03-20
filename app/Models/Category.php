e66f839bf2b42bf1037340d8a5eadcd69fbba1a3
Abdulloh Fazliddinov <abdullokh207@gmail.com>
2025-03-20 18:19:36 +0500

    category.php adding function categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -24,6 +24,10 @@ class Category extends Model
     {
         return $this->belongsTo(Category::class, 'parent_id');
     }
+    public function categories()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
     public function products()
     {
         return $this->hasMany(Product::class, 'id');
