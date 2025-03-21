999673ad495686f9aa15d7a8b1fef154ae5f7343
YoqubovDev <xiyobidasturchi0011@gmail.com>
2025-03-21 17:45:05 +0500

    patch: :)

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,4 @@ use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", [HomeController::class, "index"])->name("home");
-Route::get("/test", [ProductsController::class, "show"])->name("test");
-
+Route::get("/filter", [HomeController::class, "index"])->name("filter");
