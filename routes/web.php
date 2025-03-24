487f0a3629362ad451646c493b3d832d79a816c7
Sanjarbek <sanjarbek@example.com>
2025-03-24 18:16:01 +0500

    Remove ShopPageController and associated view

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\ShopPageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", [HomeController::class, "index"])->name("home");
+Route::get('/shop-page', [ShopPageController::class, "index"])->name("shop-page");
 Route::get("/filter", [ProductsController::class, "show"])->name("filter");
