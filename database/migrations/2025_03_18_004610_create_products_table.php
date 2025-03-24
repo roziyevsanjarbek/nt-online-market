f0943fcf251a21948558ad81ba786d9b9c75caff
Abdulaziz <salihovabdulaziz741@gmail.com>
2025-03-24 20:08:41 +0500

    :enter

--- database/migrations/2025_03_18_004610_create_products_table.php
+++ database/migrations/2025_03_18_004610_create_products_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('weight')->nullable();
             $table->string('image')->nullable();
             $table->unsignedBigInteger('category_id');
+            $table->foreign('volume_id')->references('id')->on('product_volumes')->onDelete('cascade');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
         });
