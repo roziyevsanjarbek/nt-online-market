5b5d0165c71b4291f6aa289550517fb94c75be90
Mexriddin <mekkinuriddinov@gmail.com>
2025-03-25 18:27:25 +0500

    dublicate  page deleted

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -87,71 +87,13 @@ class HomeController extends Controller
             'parentCategories' => $parentCategories
         ]);
     }
-    public function home()
-    {
-        $topBanners = Banner::query()
-            ->where('position', 'top')
-            ->get();
-        $midBanner = Banner::query()
-            ->where('position', 'middle')
-            ->latest('updated_at')
-            ->first();
-        $bottomBanner = Banner::query()
-            ->where('position', 'bottom')
-            ->latest('updated_at')
-            ->first();
-        $oneBottomBanners = Banner::query()
-            ->where('position', 'one_bottom')
-            ->latest('updated_at')
-            ->limit(2)
-            ->get();
-        $categories = Category::query()
-            ->orderBy('id', 'desc')
-            ->with(['images', 'parent'])
-            ->get();
-        $latestPosts = Post::query()
-            ->orderBy('id', 'desc')
-            ->with('postCategory')
-            ->limit(10)
-            ->get();
-        $insPosts = Post::query()
-            ->orderBy('id', 'desc')
-            ->with('insPostCategory')
-            ->limit(10)
-            ->get();
-        $parentCategories = Category::query()
-            ->whereNull('parent_id')
-            ->orderBy('id', 'desc')
-            ->limit(4)
-            ->with('categories')
-            ->get();
-        $productsMenu = Category::query()
-            ->whereNull('parent_id')
-            ->orderBy('id', 'desc')
-            ->with('categories')
-            ->get();
-        $products= Product::query()
-            ->orderBy('id', 'desc')
-            ->limit(10)
-            ->get();
-        return view('home',[
-            'topBanners' => $topBanners,
-            'midBanner' => $midBanner,
-            'bottomBanner' => $bottomBanner,
-            'oneBottomBanners' => $oneBottomBanners,
-            'categories' => $categories,
-            'latestPosts' => $latestPosts,
-            'parentCategories' => $parentCategories,
-            'insPosts' => $insPosts,
-            'products' => $products,
-            'productsMenu' => $productsMenu,
-        ]);
-    }
     public function exit()
     {
         auth()->logout();
         return redirect()->route('index');
     }
+
+
 
 
 }
