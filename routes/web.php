3ec4c5b55cce4babf4bd4e24f670d9e61f483d33
Abdulloh Fazliddinov <abdullokh207@gmail.com>
2025-03-25 17:44:28 +0500

    abdulloh branch otib rebase qilindi

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductStatusesController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,10 +11,22 @@ Route::get('/exit', [HomeController::class, 'exit'])->name('exit');
 
 Route::get('/dashboard', [HomeController::class, 'home'])->middleware('auth')->name('home');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')->group(function ()
+{
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/wishlist/toggle', [ProductStatusesController::class, 'toggleWishlist']);
+    Route::post('/cart/toggle', [ProductStatusesController::class, 'toggleCart']);
+    Route::get('/wishlist', [ProductStatusesController::class, 'wishlist']);
+    Route::get('/cart', [ProductStatusesController::class, 'cart']);
+
 });
 
 require __DIR__.'/auth.php';
