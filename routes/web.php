60c1c44905007ed41bbfcd7d3e00510cd6d76835
Mexriddin <mekkinuriddinov@gmail.com>
2025-03-25 18:27:25 +0500

    dublicate  page deleted

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,10 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('index');
-Route::get('/exit', [HomeController::class, 'exit'])->name('exit');
 
 
-Route::get('/dashboard', [HomeController::class, 'home'])->middleware('auth')->name('home');
+
+;
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
